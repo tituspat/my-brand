5e6319e1d7b96117704a1b3e70bb0c15d61e690b
Titus Patrick <tituspat0112@gmail.com>
2024-11-28 05:28:03 +0700

    feat: contacgt

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 const contactInfo = {
     email: "tituspat.me@gmail.com",
-    phone: "(+62) 812 1819 7227",
-    whatsapp: "https://wa.me/6281218197227",
+    phone: "(+62) 895-4148-74614",
+    whatsapp: "https://wa.me/62895414874614",
     address: "Surabaya, Indonesia",
 };
 
